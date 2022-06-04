42f3e84ca52a2ea730e4a3c28c58caa1a279a41c
JONGMIN KIM <bellmin9321@gmail.com>
2022-06-05 01:15:09 +0900

    Add: prettier.js

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  singleQuote: false,
+  semi: true,
+  tabWidth: 2,
+  bracketSpacing: true,
+  trailingComma: "all",
+  printWidth: 80,
+  arrowParens: "always",
+  endOfLine: "lf"
+};
