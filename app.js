249a7a3c4a9eaa8f5793ab7999d30c3077b997bc
hansol <stellanew21@gmail.com>
2022-06-05 20:42:22 +0900

    Add: install express-session & add session in app.js express-session 모듈 설치 및 app.js 파일에 세션 설

--- app.js
+++ app.js
@@ -5,16 +5,23 @@ const express = require("express");
 const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
+const session = require("express-session");
 
 const passport = require("./config/passport");
-const indexRouter = require("./routes/index");
-const usersRouter = require("./routes/users");
 const authRouter = require("./routes/auth");
 
 const app = express();
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "jade");
+
+app.use(
+  session({
+    secret: process.env.SESSION.KEY,
+    resave: false,
+    saveUninitialized: false,
+  }),
+);
 
 app.use(logger("dev"));
 app.use(express.json());
@@ -25,8 +32,7 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use(passport.initialize());
 app.use(passport.session());
 
-app.use("/", indexRouter);
-app.use("/users", usersRouter);
+app.use("/api/auth", authRouter);
 
 app.use((req, res, next) => {
   next(createError(404));
