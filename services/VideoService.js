54f40b733f251993edd53a6910161523419f549a
hansol <stellanew21@gmail.com>
2022-06-07 14:49:12 +0900

    Feat: get video DB from mongoDB and send data to frontend 데이터베이스에서 비디오 상세 정보 조회 및 프론트 전달

--- services/VideoService.js
+++ services/VideoService.js
@@ -3,3 +3,7 @@ const Video = require("../models/Video");
 exports.findVideoList = async () => {
   return await Video.find().lean();
 };
+
+exports.findVideoById = async (id) => {
+  return await Video.findById(id).lean();
+};
