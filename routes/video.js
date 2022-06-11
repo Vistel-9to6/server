2a789c582f4ef390ce1e106b732dd6166091c25c
hansol <stellanew21@gmail.com>
2022-06-12 03:19:11 +0900

    Refactor: add auth middleware in video routes and import concat/upload/delete function to controllers - 비디오 생성 및 참여 라우터에 인증 미들웨어 추가 - 비디오 컨트롤러 내 병합/업로드/삭제 등의 함수 import 처리

--- routes/video.js
+++ routes/video.js
@@ -1,13 +1,15 @@
 const express = require("express");
 const videoRouter = express.Router();
-const { getVideoList, createVideo } = require("../controllers/video");
+const {
+  getVideoList,
+  createVideo,
+  updateVideo,
+} = require("../controllers/video");
 const { uploadVideo } = require("../routes/middlewares/uploadVideo");
+const { isLoggedIn } = require("./middlewares/authorization");
 
 videoRouter.get("/", getVideoList);
-videoRouter.post(
-  "/",
-  uploadVideo.single("video"),
-  createVideo,
-);
+videoRouter.post("/", isLoggedIn, uploadVideo.single("video"), createVideo);
+videoRouter.patch("/", isLoggedIn, uploadVideo.single("video"), updateVideo);
 
 module.exports = videoRouter;
