ed599fd8484ba05f4bfe9fceeeedffe0c0b854ec
hansol <stellanew21@gmail.com>
2022-06-12 03:15:49 +0900

    Refactor: change api address and exports type - 구글 로그인 api 요청 주소 - uploadVideo 미들웨어 export 타입 변경

--- routes/middlewares/uploadVideo.js
+++ routes/middlewares/uploadVideo.js
@@ -20,11 +20,11 @@ const s3VideoUploader = multerS3({
   },
 });
 
-exports.uploadVideo = multer({
+const uploadVideo = multer({
   limits: {
     fileSize: 20 * 1024 * 1024,
   },
   storage: s3VideoUploader,
 });
 
-module.exports = { s3 };
+module.exports = { s3, uploadVideo };
