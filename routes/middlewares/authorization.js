962b0424a3d133bda9f40d13adec3b6ed30be7f2
JONGMIN KIM <bellmin9321@gmail.com>
2022-06-13 18:52:29 +0900

    Feat: jwt authorization

--- routes/middlewares/authorization.js
+++ routes/middlewares/authorization.js
@@ -1,3 +1,23 @@
-const passport = require("passport");
+const jwt = require("jsonwebtoken");
 
-exports.isLoggedIn = passport.authenticate("jwt", { session: false });
+exports.isLoggedIn = async (req, res, next) => {
+  const token = req.headers.authorization.split(" ")[1];
+
+  try {
+    if (!token) {
+      return res.status(401).json({
+        result: "ng",
+        errorMessage: "Unauthorized",
+      });
+    }
+
+    req.decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+    return next();
+  } catch (err) {
+    return res.status(403).json({
+      result: "ng",
+      errorMessage: "forbidden",
+    });
+  }
+};
